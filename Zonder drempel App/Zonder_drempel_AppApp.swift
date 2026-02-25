52033613ff450345ceec7bce527161396aedf622
Lennart Mooijweer <Mooijweer@proton.me>
2026-02-25 14:57:43 +0100

    Initial Commit

---
+++ Zonder drempel App/Zonder_drempel_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zonder_drempel_AppApp.swift
+//  Zonder drempel App
+//
+//  Created by Lennart Mooijweer on 25/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Zonder_drempel_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Zonder drempel AppTests/Zonder_drempel_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Zonder_drempel_AppTests.swift
+//  Zonder drempel AppTests
+//
+//  Created by Lennart Mooijweer on 25/02/2026.
+//
+
+import Testing
+@testable import Zonder_drempel_App
+
+struct Zonder_drempel_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
